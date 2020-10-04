d97fc8df89500feddb34a740de0e0e00bd5ee880
mfmcl <michael.mclean@student.leaf.academy>
2020-10-04 23:52:58 +0200

    Update XtremeSchoolCalc.java

--- XtremeSchoolCalc.java
+++ XtremeSchoolCalc.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class XtremeSchoolCalc {
 
     public static void main(String[] args) {
-        
+        mainMenu();
     }
 
     public static void mainMenu() {
@@ -31,10 +31,29 @@ public class XtremeSchoolCalc {
 
         switch (selectedOption) {
             case 1:
-                
+                System.out.println("case 1");
                 break;
-        
+            case 2:
+                System.out.println("case 2");
+                break;
+            case 3:
+                System.out.println("case 3");
+                break;
+            case 4:
+                System.out.println("case 4");
+                break;
+            case 5:
+                System.out.println("case 5");
+                break;
+            case 6:
+                System.out.println("case 6");
+                break;
+
             default:
+            System.out.println("---------------------------------");
+            System.out.println("-!-Please enter a valid number-!-");
+            System.out.println("---------------------------------");
+            mainMenu();
                 break;
         }
         sc.close();
