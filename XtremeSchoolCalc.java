9846355b3210a9fda9ad27b024115909642042f6
mfmcl <michael.mclean@student.leaf.academy>
2020-10-05 21:04:59 +0200

    Update XtremeSchoolCalc.java

--- XtremeSchoolCalc.java
+++ XtremeSchoolCalc.java
@@ -32,12 +32,14 @@ public class XtremeSchoolCalc {
         switch (selectedOption) {
             case 1:
                 System.out.println("case 1");
+                sleepCalc();
                 break;
             case 2:
                 System.out.println("case 2");
                 break;
             case 3:
                 System.out.println("case 3");
+                hexDecBinConverterMenu();
                 break;
             case 4:
                 System.out.println("case 4");
@@ -50,12 +52,58 @@ public class XtremeSchoolCalc {
                 break;
 
             default:
-            System.out.println("---------------------------------");
-            System.out.println("-!-Please enter a valid number-!-");
-            System.out.println("---------------------------------");
-            mainMenu();
+                System.out.println("---------------------------------");
+                System.out.println("-!-Please enter a valid number-!-");
+                System.out.println("---------------------------------");
+                mainMenu();
                 break;
         }
+        sc.close();
+    }
+
+    public static void sleepCalc() {
+        System.out.println("============================");
+        System.out.println("|     Sleep Calculator     |");
+        System.out.println("============================");
+
+    }
+
+    public static void hexDecBinConverterMenu() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("============================");
+        System.out.println("|  Hex-Dec-Bin Converter   |");
+        System.out.println("============================");
+        System.out.println("|   1. Hex to Dec & Bin    |");
+        System.out.println("|   2. Dec to Hex & Bin    |");
+        System.out.println("|   3. Bin to Hex & Dec    |");
+        System.out.println("|   4. Main Menu           |");
+        System.out.println("============================");
+        
+        System.out.println("Select option");
+        int selection = sc.nextInt();
+
+        switch (selection) {
+            case 1:
+                System.out.println("case 1");
+                break;
+            case 2:
+                System.out.println("case 2");
+                break;
+            case 3:
+                System.out.println("case 3");
+                break;
+            case 4:
+                mainMenu();
+                break;
+
+            default:
+                System.out.println("---------------------------------");
+                System.out.println("-!-Please enter a valid number-!-");
+                System.out.println("---------------------------------");
+                hexDecBinConverterMenu();
+                break;
+
         sc.close();
     }
 }
